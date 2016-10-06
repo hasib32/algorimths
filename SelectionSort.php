666e5a3ab889f56ccd9b3278628e1cdcc8b64d56
hasib32 <hasib32@gmail.com>
2016-10-06 16:35:18 -0400

    updated selection sort

--- SelectionSort.php
+++ SelectionSort.php
@@ -1,6 +1,6 @@
 <?php
 
-$unsortedArray = [-17,3,5,7,21,1,2,8,-1,5];
+$unsortedArray = [-17,3,5,7,21,1,2,8,-1,5,51,5,2,3];
 
 for($i = 0; $i < count($unsortedArray);) {
 
@@ -9,14 +9,15 @@ for($i = 0; $i < count($unsortedArray);) {
 
     for($j = $i; $j < count($unsortedArray);) {
         if (isset($unsortedArray[$j+1]) ) {
+            //find the minimum number
             if($min > $unsortedArray[$j+1]) {
                 $min = $unsortedArray[$j+1];
-                $minIndexFoundAt = $j+1;
+                $minIndexFoundAt = $j+1;//minimum number index
             }
         }
         $j++;
     }
-
+    //swapping happening here
     $temp = $unsortedArray[$i];
     $unsortedArray[$i] = $min;
     $unsortedArray[$minIndexFoundAt] = $temp;
