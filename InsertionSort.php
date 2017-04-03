4441d92527b6a187315dc11b7646631047e37316
hasib32 <hasib32@gmail.com>
2017-04-03 18:15:31 -0400

    added insertation sort

--- InsertionSort.php
+++ InsertionSort.php
@@ -1,7 +1,17 @@
 <?php
 
-$unsortedArray = [10, 7, 3];
+$unsortedArray = [14, 33, 27, 10];
 
-for($i=1; $i < count($unsortedArray); $i++) {
+for($i=0; $i < count($unsortedArray); $i++) {
+    $minNumber = $unsortedArray[$i];
+    $j = $i - 1;
+    // This part is looping until j is greater than or equal 0 and current j is greater than min
+    while ($j >= 0 && $unsortedArray[$j] > $minNumber) {
+        $unsortedArray[$j + 1] = $unsortedArray[$j];
+        $j--;
+    }
 
+    $unsortedArray[$j + 1] = $minNumber;
 }
+
+var_dump($unsortedArray);
