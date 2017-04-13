f92f45d8265309b83e2d0dfabbf4b634a3f79782
hasib32 <hasib32@gmail.com>
2017-04-13 11:21:10 -0400

    added recursive function

---
+++ Factorial.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Factorial with loop
+ *
+ * @param $number
+ * @return int
+ */
+function factorial($number)
+{
+    $result = 1;
+
+    for ($i = $number; $i >= 1; $i--) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+/**
+ * Factorial with recursive way
+ *
+ * @param $number
+ * @return int
+ */
+function recursiveFactorial($number)
+{
+    $result = 1;
+
+    if ($number >= 1) {
+        $result = $number * recursiveFactorial($number - 1);
+    }
+    
+    return $result;
+}
+
+echo recursiveFactorial(6) . "\n";
